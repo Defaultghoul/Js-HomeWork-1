beaeda89335e2c30450de06749d2c28b4b988944
defaultghoul <dang6r.senpai@gmail.com>
2023-06-27 16:32:31 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// 1
+
+let age = 12;
+console.log(age);
+
+// 2
+
+var name = 'Svyatoslav';
+console.log(name);
+
+// 3
+
+var isStudent = false;
+console.log(isStudent);
+
+// 4
+
+var myString =
+	'Не буває безвихідних ситуацій. Бувають ситуації, вихід із яких тебе не влаштовує. (Нара Шикамару)';
+console.log(myString);
+
+// 5
+
+let myNumber = 993;
+myNumber -= 10;
+
+console.log(myNumber);
+
+// 6
+
+var myNull = null;
+console.log(myNull);
+
+// 7 (Тут розкоментувати)
+
+// var yourName = prompt('Введіть своє імя:');
+
+// alert('Привіт, ' + yourName + '!');
+
+// 8 (Тут розкоментувати)
+
+// var confirm = confirm('Підтвердіть:');
+
+// if (confirm == true) {
+// 	alert('Дякую за підтвердження!');
+// } else {
+// 	alert('Дію відмінено!');
+// }
+
+// 9 (Тут розкоментувати)
+
+// alert('Дія небезпечна!');
+// var confirm = confirm('Підтвердіть:');
+
+// if (confirm == true) {
+// 	alert('Дякую за підтвердження!');
+// } else {
+// 	alert('Дію відмінено!');
+// }
+
+// Кстаті, щоб автоматично в кінці рядка ставилась точка з комою в настройках потрібно додати рядок -  	"prettier.semi": true
